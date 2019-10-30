a8f04fc8b445a4301938f043fcfbc756a9ba6a8f
Paul T <ptsouchlos@bellsouth.net>
2019-10-29 22:18:51 -0400

    Update Readme and Minor Changes to Scraper Code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kiranico-scraper
 A web-scraper script for getting MHW data from Kiranico.
+
+This is a *very* work in progress project. The main goal of this project is to be able to pull data from kiranico that is not yet available elsewhere for the Iceborne expansion of MHW. 
+
+## Requirements
+
+To run the script you will need:
+
+* Python 3 (tested with `3.8+`)
+* Python packages (install with pip if missing)
+  * `requests`
+  * `bs4`
+  * `urllib`
+  * `csv`
+
+## Related Projects
+
+This project is inspired/motivated by other similar projects, most notably the [MHWorldData](https://github.com/gatheringhallstudios/MHWorldData) project. Other projects include:
+* [MHW DB API](https://github.com/LartTyler/MHWDB-Docs/wiki)
--- kiranico-scraper.py
+++ kiranico-scraper.py
@@ -4,7 +4,17 @@ import time
 from bs4 import BeautifulSoup
 import csv 
 
-def remove(value, deletechars):
+def remove(value=str, deletechars=str) -> str:
+    """
+    Removes the given characters from the given string.
+
+    Parameters:
+    -----------
+    value: str
+        String to modify
+    deletechars: str
+        The characters to remove
+    """
     for c in deletechars:
         value = value.replace(c,'')
     return value
@@ -19,38 +29,110 @@ def download_image(url=str, output_dir=str, file_name=''):
     with open(local_path, 'wb') as outfile:
         outfile.write(results.content)
 
-### main script ###
+def process_weapon_page(page_url=str):
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    project_info = soup.find(class_='project-info')
+    description = project_info.find(class_ = 'col-sm-6')
 
+    data_table = project_info.find(class_='balance-table')
+    cells = data_table.find_all('td')
+    weapon_data = {}
+    for cell in cells:
+        label = cell.find(class_='balance-label smaller lighter text-nowrap')
+        if label != None:
+            value = cell.find('strong')
+            if value != None:
+                value_text = value.text
+                label_value = label.text.lower().strip()
+                if label_value == 'attack':
+                    attack, true_raw = value_text.split('|')
+                    weapon_data['attack'] = int(attack.strip())
+                    weapon_data['true_raw'] = int(true_raw.strip())
+                elif label_value == 'rarity':
+                    weapon_data[label_value] = int(value_text.replace('Rarity', ''))
+                elif label_value == 'affinity':
+                    weapon_data[label_value] = int(value_text.replace('%', ''))
+                elif label_value == 'element':
+                    # split element type and amount
+                    element_string = value_text.lower()
+                    weapon_data['element_hidden'] = False
+                    weapon_data['element_type'] = ''
+                    weapon_data['element_amount'] = 0
+                    if len(element_string) > 4:
+                        if value_text.find('(') != -1:
+                            weapon_data['element_hidden'] = True                           
+
+                        split_location = -1
+                        process_string = remove(element_string, '()\n').replace(' ', '').strip()
+                        for i, c in enumerate(process_string):
+                            if c.isdigit():
+                                split_location = i
+                                break
+                        if split_location > 0:
+                            element_type = process_string[0:split_location]
+                            element_amount = int(process_string[split_location+1:])
+                            weapon_data['element_type'] = element_type
+                            weapon_data['element_amount'] = element_amount
+                else:
+                    weapon_data[label_value] = value_text.strip().replace('\n', '').replace(' ', '')
+
+    return weapon_data
+
+
+def process_weapon_row(value, weapon_type=str):
+    use_type = weapon_type.replace(' ', '-').lower()
+    img_src = row.find(class_='img-fluid')
+    name = row.find(class_='d-none d-xl-inline-block')
+    
+    if name != None:
+        weapon_link = name.find('a').get('href')
+        print("Weapon link: {}".format(weapon_link))
+
+        return process_weapon_page(weapon_link)
+    
+    return None
+
+    # WIP
+    # TODO: Take into account rows that don't have a link (i.e. MR weapons from IB)
+    info_columns = row.find(class_='text-center')
+
+    if info_columns != None:
+        for column in info_columns:
+            if column.find('ruby') != -1 and column.find('rt') != -1:
+                bloated_attack = column.next_element
+                raw_attack = column.find('rt')
+                if bloated_attack != None and raw_attack != None and name != None:
+                    print("{} | {} | {}".format(name.find('a').text, bloated_attack, raw_attack.text))
+                    
+                    if img_src != None:
+                        img_link = img_src.get('src')
+                        return {
+                            'id': -1,
+                            'image_link' : img_link, 
+                            'name': name.find('a').text, 
+                            'attack' : bloated_attack, 
+                            'true_raw':raw_attack.text,
+                            'weapon_type': use_type
+                            }
+
+### main script area ###
+### ---------------- ###
 number_of_weapons = 14
 url = 'https://mhworld.kiranico.com/weapons'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-element_headers = soup.find_all(class_='element-header')
-for header in element_headers:
-    print(header)
+tables = soup.find_all(role='tabpanel')
 
-rows = soup.findAll('tr')
-download_files = False
+table_columns = ['id','name_en', 'weapon_type', 'previous_en', 'category', 'rarity', 'attack', 'affinity', 'defense', 'element_hidden', 'element1', 'element1_attack', 'element2', 'element2_attack', 'elderseal', 'slot_1', 'slot_2', 'slot_3' ,'kinsect_bonus' ,'phial' ,'phial_power', 'shelling', 'shelling_level', 'notes', 'ammo_config', 'skill']
 
-with open('data.csv', 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
+for table in tables:
+    element_header = table.find(class_='element-header')
+    weapon_type = element_header.text
+    print(weapon_type)
+    rows = table.find_all('tr')
     for row in rows:
-
-        img_src = row.find(class_='img-fluid')
-        name = row.find(class_='d-none d-xl-inline-block')
-        info_columns = row.find(class_='text-center')
-
-        if info_columns != None:
-            for column in info_columns:
-                if column.find('ruby') != -1 and column.find('rt') != -1:
-                    bloated_attack = column.next_element
-                    raw_attack = column.find('rt')
-                    if bloated_attack != None and raw_attack != None and name != None:
-                        print("{} | {} | {}".format(name.find('a').text, bloated_attack, raw_attack.text))
-
-                        if img_src != None and download_files:
-                            img_link = img_src.get('src')
-                            download_image(img_link, 'thumbs', "{}.png".format(name.find('a').text))
-                        csv_writer.writerow([name.find('a').text, bloated_attack, raw_attack.text])
-                        csv_file.flush()
+        results = process_weapon_row(row, weapon_type)
+        if results != None:
+            print(results)
