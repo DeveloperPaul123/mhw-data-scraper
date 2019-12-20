7f35d9955d5fe43801f218e92fc075f12e1d0e88
Paul T <ptsouchlos@bellsouth.net>
2019-12-20 11:03:19 -0500

    Minor Updates

--- kiranico-monster-page-scraper.py
+++ kiranico-monster-page-scraper.py
@@ -16,33 +16,65 @@ def pretty_print_monster(mon=monster):
 def process_monster_row(row, monster_type=str) -> monster:
     return_monster = monster
     columns = row.find_all('td')
+    name = columns[0].find('a').text
+    monster_link = columns[0].find('a').get('href')
+
     if len(columns) == 3:
         # iceborn data
-        name = columns[0].find('a').text
         return_monster.name = remove_characters(name, '\n').strip()
+        return_monster.image_link = None
     else:
         # base mhw data
-        name = columns[0].find('a').text
         return_monster.name = remove_characters(name, '\n').strip()
         img_src = row.find(class_='img-fluid')
         img_link = img_src.get('src')
         return_monster.image_link = img_link
+
+    if monster_link is not None:
+        process_monster_page(monster_link, return_monster)
+
     return return_monster
 
-def process_monster_page(page_url=str):
-    return None
+def process_monster_page(page_url=str, mon=monster):
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    project_info = soup.find(class_='project-info')
+    description = project_info.find(class_='col-sm-6').text
+    
+    mon.description = remove_characters(description, '\n').strip()
+
+    balance_table = project_info.find(class_='balance-table')
+
+    # find all columns in the balance table
+    balance_table_rows = balance_table.find_all('td')
+
+    # set the species
+    mon.species = remove_characters(balance_table_rows[0].text, '\n').strip()
+
+    return
 
 
+#--script area--#
+
+# base url to download data from
 url = "https://mhworld.kiranico.com/monsters"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
+# data is organized in tables on the website
 tables = soup.find_all(class_='table table-padded')
 monster_list = list()
 
+# change to True to download thumbnail images (where available) for the monsters.
+download_images = False
 for table in tables:
     rows = table.find_all('tr')
     for row in rows:
         current_monster = process_monster_row(row)        
-        print('{} {}'.format(current_monster.name, current_monster.image_link))
+        print('{} {} {}'.format(current_monster.name, current_monster.species, current_monster.image_link))
+        if download_images and current_monster.image_link is not None:
+            # download the thumbnail images for the monsters (from common.py)
+            download_image(current_monster.image_link, 'images/monsters', '{}_thumbnail.png'.format(current_monster.name))
+            
